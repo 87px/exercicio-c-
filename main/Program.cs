a35ea78589a3f14c15553e62ede291a7d6964d7e
Victor <we5705988@gmail.com>
2024-05-16 12:34:22 -0300

    Verificação de idade

---
+++ main/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        Console.WriteLine("Informe a sua idade: ");
+        String idadeString = Console.ReadLine()!; // Utilize null forgiveness apenas para desenvolvimento e debug
+        int idade = Convert.ToInt32(idadeString);
+        String mensagem = idade < 18 ? "Menor de idade" : "Maior de idade";
+        Console.WriteLine(mensagem);
+    }
+}
+
+
+
